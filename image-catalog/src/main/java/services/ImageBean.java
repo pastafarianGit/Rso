9d48434df351b5ba0ce5753716b160c8555f7545
Robert <robikoser@gmail.com>
2019-10-12 15:44:01 +0200

    Add ImageBean

---
+++ image-catalog/src/main/java/services/ImageBean.java
@@ -0,0 +1,24 @@
+package services;
+
+import models.ImageEntity;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.logging.Logger;
+
+
+@ApplicationScoped
+public class ImageBean {
+    private Logger log = Logger.getLogger(ImageBean.class.getName());
+
+    @PersistenceContext(unitName = "images-jpa")
+    private EntityManager em;
+
+    public List<ImageEntity> getRealEstateList(){
+        Query query = em.createNamedQuery("Image.getAll", ImageEntity.class);
+        return query.getResultList();
+    }
+}
