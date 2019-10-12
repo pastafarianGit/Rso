a274900c05a5805a9cc71ef64bbdb6a63d153f6b
Robert <robikoser@gmail.com>
2019-10-12 15:41:58 +0200

    Add ImageEntity

---
+++ image-catalog/src/main/java/models/ImageEntity.java
@@ -0,0 +1,79 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+@NamedQueries(value =
+        {
+                @NamedQuery(name = "ImageEntity.getAll", query = "SELECT im FROM ImageEntity im")
+        })
+public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "width")
+    private Integer width;
+
+    @Column(name = "height")
+    private Integer height;
+
+    @Column(name = "uri")
+    private String uri;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
+
